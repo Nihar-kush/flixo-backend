695fe737ea365b3c8484c3278cb0f7786f8f9904
nihar kushwaha <niharkushwaha7@gmail.com>
2023-08-24 00:53:23 +0530

    url update

--- routes/movies.js
+++ routes/movies.js
@@ -13,24 +13,32 @@ router.get("/api/movies", function (req, res, next) {
   let page = req.query.page || 1;
   let category = req.query.category;
   let finalResults = [];
-
-  let url = `https://themoviesflix.cx/category/${category}/page/${page}`;
+  
+  let url;
+  let url1 = `https://moviesflixer.lat/category/${category}/page/${page}`;
+  let url2 = `https://moviesflixer.lat`;
+  
+  if (category) {
+    url = url1;
+  } else {
+    url = url2;
+  }
+  console.log(url);
   rs(url, (err, result, html) => {
     if (!err) {
       let $ = cheerio.load(html);
-      $("#content_box")
-        .children()
+      $("#content")
+        .find(".content")
         .each((index, element) => {
           let item = {
             id: uuid(),
-            downloadLink: $(element).find("a.post-image").attr("href"),
+            title: $(element).find(".entry-title").find("a").text(),
+            downloadLink: $(element).find(".entry-title").find("a").attr("href"),
             img_url: $(element).find("img").attr("src"),
-            title: $(element).find(".title").children("a").text(),
           };
           finalResults.push(item);
         });
-      $("#menu-item-41")
-        .find(".sub-menu")
+      $(".sub-menu")
         .children()
         .each((index, element) => {
           let item1 = {
@@ -42,6 +50,34 @@ router.get("/api/movies", function (req, res, next) {
       res.send(finalResults);
     }
   });
-});
+  });
+
+
+  // rs(url, (err, result, html) => {
+  //   if (!err) {
+  //     let $ = cheerio.load(html);
+  //     $("#content_box")
+  //       .find(".latestPost")
+  //       .each((index, element) => {
+  //         let item = {
+  //           id: uuid(),
+  //           downloadLink: $(element).find(".title").find("a").attr("href"),
+  //           img_url: $(element).find(".featured-thumbnail").find("img").attr("src"),
+  //           title: $(element).find(".title").find("a").text(),
+  //         };
+  //         finalResults.push(item);
+  //       });
+  //     $(".sub-menu")
+  //       .children()
+  //       .each((index, element) => {
+  //         let item1 = {
+  //           category: $(element).find("a").text(),
+  //           categoryLink: $(element).find("a").attr("href"),
+  //         };
+  //         finalResults.push(item1);
+  //       });
+  //     res.send(finalResults);
+  //   }
+  // });
 
 module.exports = router;
